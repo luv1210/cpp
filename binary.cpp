16c4daed9ad664ac24620e1967230eb5436581bd
DayaKhichadiya <khichadiyadayam@gmail.com>
2024-12-04 09:27:06 +0530

    04/12/2024

---
+++ binary.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+    int x;
+    int y;
+    public:
+    Complex()
+    {
+        x=0;
+        y=0;
+    }
+    Complex(int a,int b)
+    {
+        x=a;
+        y=b;
+    }
+    Complex operator-(Complex z){
+        Complex t;
+        t.x=x-z.x;
+        t.y=y-z.y;
+        return t;
+  }
+  void printdata()
+  {
+    cout<<"value of x:"<<x<<endl;
+    cout<<"value of y:"<<y<<endl;
+  }
+
+};
+int main()
+{
+    class Complex s1,s2,s3;
+    s1=Complex(50,40);
+    s2=Complex(30,20);
+    cout<<"first object:"<<endl;
+    s1.printdata();
+    cout<<"second object:"<<endl;
+    s2.printdata();
+    cout<<"final output:"<<endl;
+    s3=s1-s2;
+    s3.printdata();
+    return 0;
+}
